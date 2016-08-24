807cef92988cdbeb014289f8f1fd56b255b2436b
dianyingx007 <dianyingx007@163.com>
2016-08-24 21:55:42 +0800

    采用已有meta标签viewport设置

--- flexible.js
+++ flexible.js
@@ -1,3 +1,20 @@
-;(function(win,lib){
+;(function(win){
+	//变量：
+	var doc = win.document;
+	var docEL = doc.documentElement;
+	var metaEL = doc.querySelector('meta[name="viewport"]');
+	var dpr = 0;
+	var scale = 0;
+	var timeoutid;
 
-})(window,window['lib']||(window['lib'] = {}));
+	//采用已有meta标签viewport设置
+	if (metaEL) {
+		var match = metaEL.getAttribute('content').match(/initial\-scale=()/);
+		if (match) {
+			scale = parseFloat(match[1]);
+			dpr = parseInt(1/scale);
+		}
+	}
+
+
+})(window);
