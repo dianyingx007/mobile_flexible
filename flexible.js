088ae4984432e7d4b1e35998bc975ca9f1384b3f
dianyingx007 <dianyingx007@163.com>
2016-08-24 22:29:51 +0800

    没有设置meta时，自动设置dpr和scale

--- flexible.js
+++ flexible.js
@@ -9,11 +9,29 @@
 
 	//采用已有meta标签viewport设置
 	if (metaEL) {
-		var match = metaEL.getAttribute('content').match(/initial\-scale=()/);
+		var match = metaEL.getAttribute('content').match(/initial\-scale=([\d\.]+)/);
 		if (match) {
 			scale = parseFloat(match[1]);
 			dpr = parseInt(1/scale);
 		}
+	}
+
+	//没有设置meta时，自动设置dpr和scale
+	//苹果手机按devicePixelRatio设置，最大为3；其余设备采用dpr=1
+	if (!dpr) {
+		var isIphone = win.navigator.appVersion.match(/iphone/gi);
+		var devicePixelRatio = win.devicePixelRatio;
+		if (isIphone) {
+			if (devicePixelRatio >=3){
+				dpr = 3
+			}else {
+				dpr = devicePixelRatio;
+			}
+		}else {
+			dpr = 1
+			//其他设备,dpr默认为1
+		}
+		scale = 1/dpr;
 	}
 
 
