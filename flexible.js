df92a1019c8a816a349de41a5c34f745f6342ba2
dianyingx007 <dianyingx007@163.com>
2016-08-24 23:17:22 +0800

    addEventListener resize

--- flexible.js
+++ flexible.js
@@ -51,11 +51,25 @@
 			doc.write(wrap.innerHTML);
 		}
 	}
+
 	/*函数：更新rem，并添加font-size属性到html标签*/
 	function refreshRem(){
+		/*getBoundingClientRect().width相当于物理像素*/
 		var width = docEL.getBoundingClientRect().width;
+		/*width / dpr > 540等于独立像素*/
+		if (width / dpr > 540) {
+            width = 540 * dpr;
+        }
 		var rem = width/10;
 		docEL.style.fontSize = rem +'px';
 	}
+
+	/*添加resize事件,当窗口大小发生变化,调用refreshRem*/
+	win.addEventListener('resize',function(){
+		clearTimeout(timeoutid);
+		timeoutid = setTimeout(refreshRem,300);
+	},false);
+
+	/**/
 
 })(window);
